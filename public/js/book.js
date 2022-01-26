f4b5b7f5297c4a57fd6b9e9fb63d2d99d31eaf9b
mlmanti <malikabya4@gmail.com>
2022-01-26 11:57:49 +0100

    WIP:fill automatically user information in form

--- public/js/book.js
+++ public/js/book.js
@@ -14,7 +14,6 @@ async function deleteReservation() {
         .then(response => response.json());
 }
 
-
 //mettre le botton annuler enable
 
 //function get informations for the logged user
@@ -25,21 +24,23 @@ function checkedUserLogged() {
         alert("hello")
     }
     else {
-        var userlogged = document.getElementById('lastNameusers');
-        userlogged.value = userlogged.dataset.lastname
+        var loggedInUserfirstname = document.getElementById('lastname1');
+        loggedInUserfirstname.value = loggedInUserfirstname.dataset.lastname;
+        var loggedInUserlastname = document.getElementById('firstname1');
+        loggedInUserlastname.value = loggedInUserlastname.dataset.firstname;
     }
-}
-function createNthPlayer(n) {
-    let htmlPlayer = `
+
+    function createNthPlayer(n) {
+        let htmlPlayer = `
         <h5 class="mt-5">Participant ${n + 1}</h5>
         <div class="form-row mb-5"> 
             <div class="col-md-4">
                 <label>Prénom</label>
-                <input name="[players][${n}][firstName]" type="text" class="form-control">
+                <input name="[players][${n}][firstName]" id="firstname[${n}]" type="text" class="form-control"  data-firstname={{userLoggedAsPlayer.lastName}}>
             </div>
             <div class="col-md-4">
                 <label>Nom</label>
-                <input name="[players][${n}][lastName]" type="text" class="form-control">
+                <input name="[players][${n}][lastName]" id="lastname[${n}]" type="text" class="form-control"  data-lastname={{userLoggedAsPlayer.lastName}}>
             </div>
             <div class="offset-1 col-md-3">
                 <label>Date de naissance</label>
@@ -48,19 +49,19 @@ function createNthPlayer(n) {
         </div>
     `;
 
-    return htmlPlayer;
-}
+        return htmlPlayer;
+    }
 
-document.addEventListener("DOMContentLoaded", function (event) {
+    document.addEventListener("DOMContentLoaded", function (event) {
 
-    //add input text as room capacity
-    const selectPlayers = document.getElementById("nbPlayers");
+        //add input text as room capacity
+        const selectPlayers = document.getElementById("nbPlayers");
 
-    selectPlayers.addEventListener("change", (e) => {
-        const formPlayers = clearPlayers();
-        for (let i = 0; i < e.target.value; i += 1) {
-            formPlayers.insertAdjacentHTML('beforeend', createNthPlayer(i));
-        }
-    })
+        selectPlayers.addEventListener("change", (e) => {
+            const formPlayers = clearPlayers();
+            for (let i = 0; i < e.target.value; i += 1) {
+                formPlayers.insertAdjacentHTML('beforeend', createNthPlayer(i));
+            }
+        })
 
-});
+    });
